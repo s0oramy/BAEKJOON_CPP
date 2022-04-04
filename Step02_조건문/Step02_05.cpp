0cd98040ba5efefb1c2b9f3dc3f7bb58f937f60f
Soorim <soo031100@naver.com>
2022-04-04 16:25:00 +0900

    *알람 시계

--- Step02_조건문/Step02_05.cpp
+++ Step02_조건문/Step02_05.cpp
@@ -1,24 +1,17 @@
-#include <iostream>
-using namespace std;
+#include <iostream> 
+using namespace std; 
 
-int main() {
-	int H, M;
-	cin >> H >> M;
+int main() { 
+	int h, m;
+	cin >> h >> m; 
 
-	if (H > 0) {
-		if (M < 45) 
-			cout << H - 1 << ' ' << M + 15;
-
-		else if (M >= 45) 
-			cout << H << ' ' << M - 45;
-	}
-
-	else if (H == 0) {
-		if (M < 45)
-			cout << 23 << ' ' << M + 15;
-
-		else if (M >= 45)
-			cout << H << ' ' << M - 45;
-	}
-	return 0;
+	m = m - 45; 
+	if(m < 0) { 
+		m += 60; 
+		h -= 1; 
+		if(h < 0) { 
+			h += 24; 
+		} 
+	} 
+	cout << h <<" "<<m; 
 }
