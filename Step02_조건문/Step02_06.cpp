ef83e4271095f5a213e01376abddfe69305b672c
Soorim <soo031100@naver.com>
2022-04-04 16:16:35 +0900

    오븐 시계

---
+++ Step02_조건문/Step02_06.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c, temp;
+	cin >> a >> b;
+	cin >> c;
+	
+	a += c / 60;
+	b += c % 60;
+
+	if (b >= 60) {
+		a += 1;
+		b -= 60;
+	}
+	if (a >= 24) {
+		a -= 24;
+	}
+
+	cout << a << ' ' << b << endl;
+
+	return 0;
+}
