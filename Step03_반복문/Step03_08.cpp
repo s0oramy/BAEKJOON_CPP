bff9bab102c91e8c25cb52c0a2339ec490dc74f2
Soorim <soo031100@naver.com>
2022-04-07 09:50:37 +0900

    A + B -8

--- Step03_반복문/Step03_08.cpp
+++ Step03_반복문/Step03_08.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
+	int t, a, b;
+	cin >> t;
 
-	for (int i = 1 ; i <= n; i++) {
-		for (int j = 1; j <= i; j++) {
-			cout << "*";
-		}
-		cout << '\n';
+	for (int i = 1; i <= t; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << endl;
 	}
 	return 0;
 }
