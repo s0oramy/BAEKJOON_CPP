4792b4ad6ee6685c0139a5fe2e531bcc07459c12
Soorim <soo031100@naver.com>
2022-04-18 18:35:53 +0900

    *숫자의 합

--- Step06_문자열/Step06_02.cpp
+++ Step06_문자열/Step06_02.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main() {
 	int count;
-	int sum = 0;
 	cin >> count;
 
-	string nums;
-	cin >> nums;
+	int sum = 0;
+	char ch;
 
 	for (int i = 0; i < count; i++) {
-		sum += nums[i] - 48;
+		cin >> ch;
+		sum += ch - 48;
 	}
 	cout << sum << endl;
 	return 0;
